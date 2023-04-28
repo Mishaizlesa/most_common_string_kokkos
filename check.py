a7d8832f923e4fd7fb01400bdf963d26056e4ddb
mixa <mihsa11ru@gmail.com>
2023-04-28 12:19:26 +0300

    py

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ from timeit import default_timer as timer
 import time
 import os
 import sys
-num=[0]
+num=[0,1]
 inf=1000
 names=["hash3","hash3_omp","rabin_karp","rabin_karp_vect","hash3_seq","rabin_karp_seq","rabin_karp_seq_vect"]
 os.environ["OMP_PLACES"]="threads"
@@ -13,7 +13,7 @@ i=10;
 while(i<inf):
     file="genome_samples/s"+str(i)+".txt"
     for j in num:
-         subprocess.call(["build/"+names[j],file,"2"])
+         subprocess.call(["build/"+names[j],file,"2","32"])
          tm=open("tmp.txt").read()
          sample=open("ans/s"+str(i)+".txt").read()
 	 if (tm!=sample): print(tm,sample)
--- main.py
+++ main.py
@@ -7,19 +7,32 @@ import sys
 num= list(map(int, sys.argv[1:]))
 inf=num[0]
 num=num[1:]
-names=["hash3","hash3_omp","rabin_karp","rabin_karp_vect","hash3_seq","rabin_karp_seq","rabin_karp_seq_vect","naive"]
+names=["hash3","hash3_omp","rabin_karp","rabin_karp_vect","hash3_seq","naive"]
 os.environ["OMP_PLACES"]="threads"
 os.environ["OMP_PROC_BIND"]="spread"
-i=10;
-sys.stdout.write("size ")
-for j in num: sys.stdout.write(names[j]+" ")
-sys.stdout.write("\n")
+i=50;
+out={};
+for j in num:
+    out[j]=open("time_results/"+names[j]+".txt","w+")
+    out[j].write("size/pattern lenght ")
+    tmp=4;
+    while(tmp<2048):
+        out[j].write(str(tmp)+" ")
+        tmp*=2
+    out[j].write("\n")
 while(i<inf):
-    sys.stdout.write(str(i)+" ")
     file="genome_samples/s"+str(i)+".txt"
     for j in num:
-         subprocess.call(["build/"+names[j],file,"1"])
-         tm=open("tmp.txt").read()
-         sys.stdout.write(tm[:-1]+" ")
+        out[j].write(str(i)+" ")
+        tmp=4;
+        while(tmp<2048):
+            if tmp>i:
+                out[j].write(" ");
+                tmp*=2
+                continue
+            subprocess.call(["build/"+names[j],file,"1",str(tmp)])
+            tm=open("tmp.txt").read()
+            out[j].write(tm[:-1]+" ")
+            tmp*=2
+        out[j].write("\n")
     i+=i//10
-    sys.stdout.write("\n")
